bdc087752927b7c8e251e32a5f6ed674622d121e
rich-ter <me@philipposrichter.com>
2022-11-10 14:23:12 +0200

    first commit

---
+++ blog_api/views.py
@@ -0,0 +1,27 @@
+from rest_framework import generics
+from blog.models import Post
+from .serializers import PostSerializer
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAdminUser, DjangoModelPermissions
+# RETURNS ALL POSTS THAT ARE PUBLISHED CALLED FROM THE .MODELS OF BLOG
+
+
+class PostUserWritePermission(BasePermission):
+    message = 'Editing posts is restricted to the author only'
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
+        return obj.author == request.user
+
+
+class PostList(generics.ListCreateAPIView):
+    permission_classes = [DjangoModelPermissions]
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView, PostUserWritePermission):
+    permission_classes = [PostUserWritePermission]
+    queryset = Post.postobjects.all()
+    serializer_class = PostSerializer
